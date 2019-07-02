e6bb27872500dc728acccef7a1bfa2b81afb4d47
zenger7 <nawaaf.almansour@gmail.com>
2019-07-02 22:44:01 +0300

    7

--- js/app.js
+++ js/app.js
@@ -1,115 +1,54 @@
+$.idx;
+let b = 0;
+document.getElementsByTagName('audio')[0].volume = 0.3;
+var audio = document.getElementById("clickSound");
 
-
-var idx ;
-let b =0;
-var arrayX =[];
-var arrayO =[];
-let Win1=0 ,Win2=0 ,Win3=0 ,Win4=0 ,Win5=0 ,Win6=0 ,Win7=0 ,Win8=0 ;
-let WinO1=0 ,WinO2=0 ,WinO3=0 ,WinO4=0 ,WinO5=0 ,WinO6=0 ,WinO7=0 ,WinO8=0 ;
-
-
- document.getElementsByTagName('audio')[0].volume = 0.3;
- var audio = document.getElementById("clickSound");
-
- $('.optionPlay button').click(function(event) {
+$('.optionPlay button').click(function(event) {
   let select = $(this).attr("id");
-console.log(select);
+  console.log(select);
 });
 
 $(function() {
-    $('.selectBox').change(function() {
-    let z   = this.form.submit();
-        console.log(z);
-
-    });
+  $('.selectBox').change(function() {
+    let z = this.form.submit();
+    console.log(z);
+  });
 });
-
 
 $('.square').click(function(event) {
-   idx = $(this).children("div").attr("id");
-   audio.play();
+  $.idx = $(this).attr('id');
+  audio.play();
   if (b == 0) {
-    if( document.getElementById(idx).innerHTML === "" ) {
-      document.getElementById("showPlayer").innerHTML = "2Player";
-    document.getElementById(idx).innerHTML = '<i class="far fa-circle a-circle">';
-    b=1;
-
-            if("x1"  === idx || "x2" === idx || "x3" === idx){
-            WinO1 += 1; }
-            if("x4" === idx || "x5" === idx || "x6" === idx){
-              WinO2 += 1;}
-            if("x7" === idx || "x8" === idx || "x9" === idx){
-               WinO3 += 1;}
-            if("x1" === idx || "x4" === idx || "x7" === idx){
-              WinO4 += 1;}
-            if("x2" === idx || "x5" === idx || "x8" === idx){
-              WinO5 += 1;}
-            if("x3" === idx || "x6" === idx || "x9" === idx){
-              WinO6 += 1;}
-            if("x1" === idx || "x5" === idx || "x9" === idx){
-              WinO7 += 1;}
-            if("x3" === idx || "x5" === idx || "x7" === idx){
-                WinO8 += 1;}
-
-    if(WinO1 === 3 || WinO2  === 3 || WinO3  === 3 || WinO4   === 3 ||WinO5   === 3 || WinO6  === 3 || WinO7  === 3 || WinO8 === 3 ){
-      console.log("you are win");
-      $('h1').text("win O");
-     // $('body').css('background-image', 'url(' + 'wintage.jpeg' + ')cover ');
-    //$('body').css('background', 'url(wintage.jpeg) cover no-repeat ');
-    $('body').css({'background-image':'url(' + 'wintage.jpeg' + ')',
-                    'background-size': 'cover',
-                    'background-repeat': 'no-repeat'});
-
-
+    if ($.idx = $(this).text() == "") {
+      $.idx = $(this).html('<i class="far fa-circle a-circle">');
+      $.idx = $(this).attr('name', 'O');
+      checkWinner('O');
+      b = 1;
+    }} else {
+    if ($.idx = $(this).text() == "") {
+      $.idx = $(this).html('<i class="fas fa-times x-icon"></i>');
+      $.idx = $(this).attr('name', 'X');
+      checkWinner('X');
+      b = 0;
     }
-  }}else{
-    if( document.getElementById(idx).innerHTML === "" ) {
-    document.getElementById("showPlayer").innerHTML = "1Player";
-    document.getElementById(idx).innerHTML = '<i class="fas fa-times x-icon"></i>';
-    b=0;
-
-        if("x1"  === idx || "x2" === idx || "x3" === idx){
-        Win1 += 1; }
-        if("x4" === idx || "x5" === idx || "x6" === idx){
-          Win2 += 1;}
-        if("x7" === idx || "x8" === idx || "x9" === idx){
-           Win3 += 1;}
-        if("x1" === idx || "x4" === idx || "x7" === idx){
-          Win4 += 1;}
-        if("x2" === idx || "x5" === idx || "x8" === idx){
-          Win5 += 1;}
-        if("x3" === idx || "x6" === idx || "x9" === idx){
-          Win6 += 1;}
-        if("x1" === idx || "x5" === idx || "x9" === idx){
-          Win7 += 1;}
-        if("x3" === idx || "x5" === idx || "x7" === idx){
-            Win8 += 1;}
-    if(Win1 === 3 || Win2  === 3 || Win3  === 3 || Win4   === 3 ||Win5   === 3 || Win6  === 3 || Win7  === 3 || Win8 === 3 ){
-      console.log("you are win X");
-      $('h1').text(" win X");
-        $('body').css({'background-image':'url(' + 'wintage.jpeg' + ')',
-                    'background-size': 'cover',
-                    'background-repeat': 'no-repeat'});
-    }
-  }}
+  }
 });
 
+
+function checkWinner(player) {
+  if (($("#0").attr('name') == player && $("#1").attr('name') == player && $("#2").attr('name') == player) ||
+    ($("#3").attr('name') == player && $("#4").attr('name') == player && $("#5").attr('name') == player) ||
+    ($("#6").attr('name') == player && $("#7").attr('name') == player && $("#8").attr('name') == player) ||
+    ($("#0").attr('name') == player && $("#3").attr('name') == player && $("#6").attr('name') == player) ||
+    ($("#1").attr('name') == player && $("#4").attr('name') == player && $("#7").attr('name') == player) ||
+    ($("#2").attr('name') == player && $("#5").attr('name') == player && $("#8").attr('name') == player) ||
+    ($("#0").attr('name') == player && $("#4").attr('name') == player && $("#8").attr('name') == player) ||
+    ($("#2").attr('name') == player && $("#4").attr('name') == player && $("#6").attr('name') == player)) {
+    console.log("you are win "+ player);
+    $('h1').text("win " + player);
+  }
+}
 
 $('#buttonR').click(function() {
-    location.reload();
+  location.reload();
 });
-
-
-
-//Win situations
-//
-// ["x1", "x2", "x3"]
-// ["x4", "x5", "x6"]
-// ["x7", "x8", "x9"]
-//
-//  ["x1", "x4", "x7"]
-//  ["x2", "x3", "x8"]
-//  ["x3", "x6", "x9"]
-//
-//   ["x1", "x5", "x9"]
-//   ["x3", "x5", "x7"]
--- main.js
+++ main.js
@@ -1,48 +1,48 @@
 var playBord;
-const humanPlayer =  '<i class="far fa-circle a-circle">';
+const humanPlayer = '<i class="far fa-circle a-circle">';
 const humanPlayer2 = '<i class="fas fa-times x-icon"></i>';
 const robot = '<i class="fas fa-times x-icon"></i>';
-let value ;
+let value;
 let losePlayer;
 const winCombos = [
-  [0,1,2],[3,4,5],[6,7,8],[0,3,6],
-  [1,4,7],[2,5,8],[0,4,8],[6,4,2]
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+  [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]
 ]
 document.getElementsByTagName('audio')[0].volume = 0.3;
 var audio = document.getElementById("clickSound");
 let comp = 1;
-let b =0;
+let b = 0;
 const cells = document.querySelectorAll('.square');
 console.log(cells);
-let n ;
+let n;
 
 
 startGame();
 
 
-function gameMode(mode){
- document.querySelector(".optionPlay").style.display="none";
- console.log(mode);
+function gameMode(mode) {
+  document.querySelector(".optionPlay").style.display = "none";
+  console.log(mode);
   if (mode == "twoPlayer") {
-         comp = 0;
-         n =0;
-   } else if (mode == "easyMode") {
+    comp = 0;
+    n = 0;
+  } else if (mode == "easyMode") {
     comp = 1;
     value = 0;
-  } else if ( mode == "hardMode" ){
+  } else if (mode == "hardMode") {
     comp = 1;
     value = 1;
 
   }
-  }
+}
 //start Game
-function startGame(){
+function startGame() {
   n = n + 1;
   console.log(n);
-  document.querySelector(".endgame").style.display="none";
-  document.querySelector(".optionPlay").style.display="block";
-  playBord = [0,1,2,3,4,5,6,7,8];
-  for(var i = 0; i < cells.length; i++) {
+  document.querySelector(".endgame").style.display = "none";
+  document.querySelector(".optionPlay").style.display = "block";
+  playBord = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  for (var i = 0; i < cells.length; i++) {
     cells[i].innerText = '';
     cells[i].style.removeProperty('background-color');
     cells[i].addEventListener('click', turnClick, false);
@@ -50,30 +50,30 @@ function startGame(){
 }
 
 
-function turnClick(sq){
-if (comp == 0) {
-  if(!checkWin(playBord, humanPlayer) && !checkTie()){
-  if (b == 0) {
-  turn(sq.target.id, humanPlayer);
-  b = 1;
-}else{
-       turn(sq.target.id, humanPlayer2);
-       b = 0;
-     }
-   }
-}else {
-  if(typeof playBord[sq.target.id]== 'number'){
-       turn(sq.target.id, humanPlayer);
-      if(!checkWin(playBord, humanPlayer) && !checkTie()){
-            turn(bestSpot(),robot);
-        }
+function turnClick(sq) {
+  if (comp == 0) {
+    if (!checkWin(playBord, humanPlayer) && !checkTie()) {
+      if (b == 0) {
+        turn(sq.target.id, humanPlayer);
+        b = 1;
+      } else {
+        turn(sq.target.id, humanPlayer2);
+        b = 0;
+      }
+    }
+  } else {
+    if (typeof playBord[sq.target.id] == 'number') {
+      turn(sq.target.id, humanPlayer);
+      if (!checkWin(playBord, humanPlayer) && !checkTie()) {
+        turn(bestSpot(), robot);
+      }
+    }
   }
-}
 
 }
 
 
-function turn(sqId , player){
+function turn(sqId , player) {
   playBord[sqId] = player;
   audio.play();
   document.getElementById(sqId).innerHTML = player;
@@ -83,55 +83,57 @@ function turn(sqId , player){
 
 
 function checkWin(bord , player) {
-        let plays = bord.reduce((a,e,i) =>
-            (e === player) ? a.concat(i) : a,[]);
-       let gameWon = null;
-       for (let [index , win] of winCombos.entries()) {
-            if(win.every(elem => plays.indexOf(elem) > -1)) {
-               gameWon = {index: index, player: player};
-              break;
-            }
-       }
-       return gameWon;
+  let plays = bord.reduce((a, e, i) =>
+    (e === player) ? a.concat(i): a, []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = {
+        index: index, player: player
+      };
+      break;
+    }
+  }
+  return gameWon;
 }
 
 function gameOver(gameWon) {
-      for(let index of winCombos[gameWon.index]){
-          document.getElementById(index).style.backgroundColor =
-                 gameWon.player == humanPlayer ? "yellow" : "#d60f74";
-      }
-      for (var i = 0; i < cells.length; i++) {
-          cells[i].removeEventListener('click',turnClick, false);
-      }
-      declareWinner(gameWon.player == humanPlayer ? "You win!" : "You lose.");;
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+    gameWon.player == humanPlayer ? "yellow": "#d60f74";
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener('click', turnClick, false);
+  }
+  declareWinner(gameWon.player == humanPlayer ? "You win!": "You lose.");;
 }
 
 
-function declareWinner(massage){
-        document.querySelector(".endgame").style.display = "block";
-        document.querySelector(".endgame .text").innerText= massage;
+function declareWinner(massage) {
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText = massage;
 }
 
 function emptySquares() {
-        return playBord.filter(s => typeof s == 'number');
+  return playBord.filter(s => typeof s == 'number');
 }
 
 
 function bestSpot() {
-     return minimax(playBord, robot).index;
+  return minimax(playBord, robot).index;
 
 }
 
 
 
 function checkTie() {
-  if ( emptySquares().length == 0) {
-       for (var i = 0; i < cells.length; i++) {
-           cells[i].style.backgroundColor = '#81899B';
-            cells[i].removeEventListener('click',turnClick, false);
-       }
-       declareWinner("Tie Game!");
-       return true;
+  if (emptySquares().length == 0) {
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = '#81899B';
+      cells[i].removeEventListener('click', turnClick, false);
+    }
+    declareWinner("Tie Game!");
+    return true;
   }
   return false;
 }
@@ -139,84 +141,95 @@ function checkTie() {
 
 function minimax(newBoard, player) {
   var availSpots = emptySquares();
-if(value == 0){
-losePlayer = player;
-}else {
-  losePlayer = humanPlayer;
-}
-if(checkWin(newBoard, losePlayer)){
-  return {score: -10 };
- 	} else if (checkWin(newBoard, robot)) {
-  return {score: 10};
-} else if(availSpots.length === 0){
-  return {score: 0};
-}
-var moves = [];
-for (var i = 0; i < availSpots.length; i++) {
-  var move = {};
-  move.index = newBoard[availSpots[i]];
-  newBoard[availSpots[i]] = player;
-
-  if (player == robot) {
-    var result = minimax(newBoard, humanPlayer);
-    move.score = result.score;
+  if (value == 0) {
+    losePlayer = player;
   } else {
-    var result = minimax(newBoard, robot);
-    move.score = result.score;
+    losePlayer = humanPlayer;
   }
+  if (checkWin(newBoard, losePlayer)) {
+    return {
+      score: -10
+    };
+  } else if (checkWin(newBoard, robot)) {
+    return {
+      score: 10
+    };
+  } else if (availSpots.length === 0) {
+    return {
+      score: 0
+    };
+  }
+  var moves = [];
+  for (var i = 0; i < availSpots.length; i++) {
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+
+    if (player == robot) {
+      var result = minimax(newBoard, humanPlayer);
+      move.score = result.score;
+    } else {
+      var result = minimax(newBoard, robot);
+      move.score = result.score;
+    }
     newBoard[availSpots[i]] = move.index;
 
-     moves.push(move);
-}
- var bestMove;
- if(player === robot){
-   var bestScore = -10000;
-   for (var i = 0; i < moves.length; i++) {
-     if(moves[i].score > bestScore){
-       bestScore = moves[i].score;
-       bestMove =i;
-     }
-   }
- }else{
-   var bestScore = 10000;
-   for (var i = 0; i < moves.length; i++) {
-     if(moves[i].score < bestScore){
-       bestScore = moves[i].score;
-       bestMove =i;
-     }
-   }
- }
+    moves.push(move);
+  }
+  var bestMove;
+  if (player === robot) {
+    var bestScore = -10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    var bestScore = 10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
 
- return moves[bestMove];
+  return moves[bestMove];
 }
 
 
 function bannerFun(inpit) {
   if (inpit == "Player1Name") {
-  document.querySelector("#Player1Name").style.display="none";
-  document.querySelector("#Player1").style.display="block";
-}else {
-  document.querySelector("#Player2Name").style.display="none";
-  document.querySelector("#Player2").style.display="block";
-}
+    document.querySelector("#Player1Name").style.display = "none";
+    document.querySelector("#Player1").style.display = "block";
+  } else {
+    document.querySelector("#Player2Name").style.display = "none";
+    document.querySelector("#Player2").style.display = "block";
+  }
 }
 
-function keyCode(event,inpit ) {
+function keyCode(event, inpit) {
   var x = event.keyCode;
   if (x == 13) {
     if (inpit == "Player1") {
-    var text = document.getElementById("Player1").value;
-    document.getElementById("Player1Name").innerHTML = text;
-    document.querySelector("#Player1Name").style.display="block";
-  document.querySelector("#Player1").style.display="none";
-}else {
-  var text = document.getElementById("Player2").value;
-  document.getElementById("Player2Name").innerHTML = text;
-  document.querySelector("#Player2Name").style.display="block";
-document.querySelector("#Player2").style.display="none";
-}
-}
+      var text = document.getElementById("Player1").value;
+      document.getElementById("Player1Name").innerHTML = text;
+      document.querySelector("#Player1Name").style.display = "block";
+      document.querySelector("#Player1").style.display = "none";
+    } else {
+      var text = document.getElementById("Player2").value;
+      document.getElementById("Player2Name").innerHTML = text;
+      document.querySelector("#Player2Name").style.display = "block";
+      document.querySelector("#Player2").style.display = "none";
+    }
+  }
 
 }
+
+
+$('#buttonR').click(function() {
+  location.reload();
+});
 
 // https://www.freecodecamp.org/news/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37/
